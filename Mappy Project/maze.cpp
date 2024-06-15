ef04a0c3a02ae530449252ad35ae6aa366f0358b
RileyErickson <butterscotch.creampuffs@gmail.com>
2024-06-15 14:07:06 -0400

    added comments

--- Mappy Project/maze.cpp
+++ Mappy Project/maze.cpp
@@ -3,6 +3,7 @@
 #include <cstdio> 
 maze::maze(int level)
 {
+	//loads the level needed 
 	switch (level) {
 	case(1):MapLoad("level1.fmp", 1);break;
 	case(2):MapLoad("level2.fmp", 1); break;
@@ -13,12 +14,14 @@ maze::~maze()
 {
 
 }
+//draws the maze
 void maze::drawMaze() {
 	MapDrawBG(0, 0, 0, 0, 1248 - 1, 768 - 1);
 	MapDrawFG(0, 0, 0, 0, 1248 - 1, 768 - 1, 0);
 }
 bool maze::getWall(int x, int y) {
-	
+	//checks if  any of the 4 corners of the character are in a wall
+	// x and y are the cords of the character
 	//top left
 	blockdata = MapGetBlock((x+15) / mapblockwidth,(y+15) / mapblockwidth);
 	if (blockdata->tl) {
