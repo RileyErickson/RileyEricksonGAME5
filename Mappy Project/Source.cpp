02eeb5e92e0777e493df1e01523e833b79d6ef32
RileyErickson <butterscotch.creampuffs@gmail.com>
2024-06-15 14:05:26 -0400

    added comments

--- Mappy Project/Source.cpp
+++ Mappy Project/Source.cpp
@@ -32,11 +32,16 @@ int main()
     al_register_event_source(event_queue, al_get_keyboard_event_source());
     maze* m = new maze(level);
     Character WizardRat;
+    //places main character at starting position
     WizardRat.InitChar(10, 3 * 92);
     al_start_timer(timer);
+    //main game loop
     while (!done) {
         ALLEGRO_EVENT ev;
         al_wait_for_event(event_queue, &ev);
+        //checks if key has been pressed and if there is a wall in the wall
+        //2 means dont flip character 1 means face left o means face right
+        //corrasponds to allegro flags;
          if ((keys[UP])&&!(m->getWall(WizardRat.getX(), WizardRat.getY() - 3)))
             WizardRat.UpdateChar(0, -3, 2);
          if ((keys[DOWN]) && !(m->getWall(WizardRat.getX(), WizardRat.getY() + 3)))
@@ -48,6 +53,7 @@ int main()
 
         if (ev.type == ALLEGRO_EVENT_TIMER){
         }
+        //handles movement
         else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
         {
             switch (ev.keyboard.keycode)
@@ -85,15 +91,19 @@ int main()
                 break;
             }
         }
+        //exit conditions if they run out of time or complte all 4 levels
         if (al_get_time() >= 60) { done = true; }
         if (level == 4) { done = true; }
+        //if the rat touches the right side of the screen then they beat that level
         if(WizardRat.getX() + 100 >= 1248) {
-            std::cout << "end of level"<<std::endl;
             level++;
+            //resets the character
             WizardRat.InitChar(10, 3 * 92);
+            //loads the next level
             if(level != 4)
                 m = new maze(level);
         }
+        //draws the screen
         m->drawMaze();
         WizardRat.DrawChar();
         al_draw_filled_rectangle(96 * 12, 0, 96 * 13, 96, al_map_rgb(0, 0, 0));
@@ -102,6 +112,7 @@ int main()
         al_flip_display();
 
     }
+    //displays final score or how far they got
     al_destroy_display(display);
     display = al_create_display(900, 400 );
     al_draw_filled_rectangle(0, 0, 900, 400, al_map_rgb(0, 200, 200));
