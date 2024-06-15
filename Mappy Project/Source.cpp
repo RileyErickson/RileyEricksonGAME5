1a068a5b2cd42c30c0d3dec9a61a73d4eb2d0c7d
riley <riley@DESKTOP-RCGACV5>
2024-06-15 00:16:36 -0400

    removing test codeadded collison

--- Mappy Project/Source.cpp
+++ Mappy Project/Source.cpp
@@ -86,7 +86,7 @@ int main()
             std::cout << "HIT" << std::endl;
 
         }
-
+        //test
         if (true) {
             //TL
             al_draw_filled_circle(WizardRat.getX()+15, WizardRat.getY()+15, 5, al_map_rgb(255, 0, 0));
