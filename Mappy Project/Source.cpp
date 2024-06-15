011408c0ec58bb17bbfc8bbf39f3a77dea7a1c8b
RileyErickson <butterscotch.creampuffs@gmail.com>
2024-06-14 22:38:36 -0400

    fixed Divergence
    I dont know what caused it but VS is no longer willing to push changes hope this will fix it

--- Mappy Project/Source.cpp
+++ Mappy Project/Source.cpp
@@ -9,22 +9,81 @@
 using namespace std;
 int main()
 {
+    bool keys[] = { false, false, false, false };
+    enum KEYS { UP, DOWN, LEFT, RIGHT, SPACE };
     al_init();
     al_install_keyboard();
     al_init_image_addon();
     int width = 1248;
     int height = 768;
     ALLEGRO_DISPLAY* display = al_create_display(width, height); 
-    ALLEGRO_EVENT_QUEUE* event_queue = NULL;
-    ALLEGRO_TIMER* timer;
-    Character Rat;
-     if (MapLoad("level1.fmp", 1))
-        return -5;
-    Rat.InitChar(100, 0);
-    while (true) {        
-        MapDrawBG(0, 0, 0, 0, width-1, height-1);
-        MapDrawFG(0, 0, 0, 0, width - 1, height - 1, 0);
-        al_rest(0.1);
+    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60);
+    
+
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    maze m(1);
+    Character WizardRat;
+    WizardRat.InitChar(0, 3 * 92);
+    al_start_timer(timer);
+    while (true) {
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);
+         if (keys[UP])
+            WizardRat.UpdateChar(0, -3, 2);
+         if (keys[DOWN])
+            WizardRat.UpdateChar(0, 3, 2);
+         if (keys[LEFT])
+            WizardRat.UpdateChar(-3, 0, 1);
+         if (keys[RIGHT])
+            WizardRat.UpdateChar(3, 0, 0);
+
+        if (ev.type == ALLEGRO_EVENT_TIMER){
+        }
+        else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+        {
+            switch (ev.keyboard.keycode)
+            {
+            case ALLEGRO_KEY_ESCAPE:
+                break;
+            case ALLEGRO_KEY_UP:
+                keys[UP] = true;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                keys[DOWN] = true;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                keys[LEFT] = true;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                keys[RIGHT] = true;
+                break;
+            }
+        }
+        if (ev.type == ALLEGRO_EVENT_KEY_UP) {
+            switch (ev.keyboard.keycode)
+            {
+            case ALLEGRO_KEY_UP:
+                keys[UP] = false;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                keys[DOWN] = false;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                keys[LEFT] = false;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                keys[RIGHT] = false;
+                break;
+            case ALLEGRO_KEY_SPACE:
+                keys[SPACE] = false;
+                break;
+            }
+        }
+          
+        m.drawMaze();
+        WizardRat.DrawChar();
         al_flip_display();
 
     }
